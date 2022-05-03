3172383992818d595d456135bccae9a92aa08dbe
Akshay kadam <akshay.kadam@excellarate.com>
2022-05-03 17:53:19 +0530

    added some form fileds

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup,FormBuilder,Validators } from '@angular/forms';
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.css']
 })
 export class AdminComponent implements OnInit {
-
+  adminForm!:FormGroup;
   constructor() { }
 
   ngOnInit(): void {
